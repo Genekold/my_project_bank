33f240c9571687a065b082bfec7f9db5e7672d57
mihaylovea <mihailovevgenya@gmail.com>
2024-05-28 10:37:42 +0300

    add tests modules

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ from src.widget import card_account, data_view
 from src.processing import get_list_by_key, sort_list_by_data
 
 if __name__ == '__main__':
-    print(card_account('Visa Gold 5999414228426353'))
+    print(card_account('Visa Gold 5992332392388329'))
     print(data_view('2018-07-11T02:26:18.671407'))
     print(get_list_by_key([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-                            {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-                            {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-                            {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]))
+                           {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+                           {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+                           {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]))
     print(sort_list_by_data([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
                            {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
                            {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
---
+++ tests/conftest.py
@@ -0,0 +1,19 @@
+import pytest
+
+
+@pytest.fixture
+def number_card():
+    return "Visa Gold 5999414228426353"
+
+
+@pytest.fixture
+def number_account():
+    return "Счет 64686473678894779589"
+
+
+@pytest.fixture
+def list_by():
+    return ([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+         {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+         {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+         {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}])
