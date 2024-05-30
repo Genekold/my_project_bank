32c233418cc0be00a221d950296cb2e144595f66
mihaylovea <mihailovevgenya@gmail.com>
2024-05-30 22:06:43 +0300

    правки после проверки наставником

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -2,6 +2,7 @@ import pytest
 
 from src.masks import mask_card, mask_bank_account
 
+
 def test_mask_card():
     assert mask_card("1234098765432567") == "1234 09** **** 2567"
 
@@ -9,7 +10,6 @@ def test_mask_card():
 
     with pytest.raises(TypeError):
         mask_card(123231321231)
-
 
 
 def test_mask_bank_account():
