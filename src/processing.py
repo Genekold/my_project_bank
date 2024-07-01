4e5c66f0dd9d9b0e1bf60c3382cf7500b7257a67
mihaylovea <mihailovevgenya@gmail.com>
2024-07-01 07:42:56 +0300

    добавлен функционал фильтрации операций по описанию

--- src/processing.py
+++ src/processing.py
@@ -1,3 +1,6 @@
+import re
+
+
 def get_list_by_key(operations: list, state: str = "EXECUTED") -> list:
     """Функция вернет список словарей по ключу (state)"""
 
@@ -15,3 +18,22 @@ def sort_list_by_data(operations: list, ascending: bool = True) -> list:
     sort_list = sorted(operations, key=lambda operation: operation["date"], reverse=ascending)
 
     return sort_list
+
+
+def get_filter_list_by_description(list_operations: list[dict], search_line: str) -> list[dict]:
+    """
+     Функция фильтрует банковские операции по строке введеной пользователем
+    :param list_operations: Список словарей о банковских операций.
+    :param search_line: Строка поиска.
+    :return: Список словарей в которой есть в отисании строка поиска.
+    """
+    filtr_operation = []
+    for operation in operations:
+        descript = operation.get('description')
+        try:
+            match = re.search(search_line, descript, re.IGNORECASE)
+            if match:
+                filtr_operation.append(operation)
+        except Exception:
+            continue
+    return filtr_operation
